ca30475c0332160e6799fefce9d0520429a0f820
tspyder7 <navneet.davang.dev@gmail.com>
2025-06-01 21:59:05 +0530

    feat: added cli configuration for boilerforge

---
+++ src/config/cli-config.ts
@@ -0,0 +1,20 @@
+type SemVersion =
+    | `${number}.${number}.${number}`
+    | `${number}.${number}.${number}-${string}`
+    | `${number}.${number}.${number}+${string}`
+    | `${number}.${number}.${number}-${string}+${string}`;
+
+type CLIConfiguration = {
+    name: string;
+    description: string;
+    version: SemVersion;
+};
+
+const cliConfig: CLIConfiguration = {
+    name: 'boilerforge',
+    description:
+        'boilerforge is a blazing-fast CLI utility that scaffolds clean, ready-to-use project structures so you can skip the setup and start building instantly.',
+    version: '0.0.1',
+};
+
+export default cliConfig;
