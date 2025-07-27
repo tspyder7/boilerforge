3513f13c4948a452236a949bc7a7a319bf45eb34
tspyder7 <navneet.davang.dev@gmail.com>
2025-07-27 22:43:55 +0530

    chore: updated package version (#27)

--- src/config/cli-config.ts
+++ src/config/cli-config.ts
@@ -14,7 +14,7 @@ const cliConfig: CLIConfiguration = {
     name: 'boilerforge',
     description:
         'boilerforge is a blazing-fast CLI utility that scaffolds clean, ready-to-use project structures so you can skip the setup and start building instantly.',
-    version: '0.0.1',
+    version: '1.0.0',
 };
 
 export default cliConfig;
